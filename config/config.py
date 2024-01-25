90262d8fc22fecddc16c126e4baf9b5339c1f59c
Khiev Boraty <59820925+tykhiev@users.noreply.github.com>
2024-01-25 17:24:33 +0700

    add weaviate key to env

--- config/config.py
+++ config/config.py
@@ -5,6 +5,9 @@ load_dotenv(find_dotenv())
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+WEAVIATE_API_KEY = os.getenv('WEAVIATE_API_KEY')
+WEAVIATE_URL = os.getenv('WEAVIATE_URL')
+MISTRAL_API_KEY = os.getenv('MISTRAL_API_KEY')
 
 DB_DIR = 'data/db'
 OUTPUT_DIR = 'data/output'
